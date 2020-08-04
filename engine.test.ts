be727df600ba6df7a9b60495ffd3837b9925b92f
Johan <nissejokke@gmail.com>
2020-08-04 21:28:30 +0200

    Working single and multiple word search

--- engine.test.ts
+++ engine.test.ts
@@ -3,6 +3,28 @@ import { Engine } from "./engine";
 describe('Search', () => {
     let engine: Engine;
     beforeEach(() => {
+        /*
+            urlToSite: {
+                "/botanik": 1
+            }
+
+            site = {
+                1: {
+                    url: "/botanik",
+                    rank: 10,
+                },
+                2000: {
+                    url: "/växt",
+                    rank: 100
+                },
+            }
+
+            index = {
+                "botanik": [1,..2000],
+                "växt": [50.., 2000],
+                "site:/botanik": [1,4,6,7,8,9,..,200]
+            }
+        */
         engine = new Engine();
         engine.add({
             text: 'Botanik (av grekiska βοτανική, "som rör örter";[1] även fytologi, "läran om växter") är det område inom biologin som studerar växtriket. Botanik omfattar tillväxt, reproduktion, ämnesomsättning, utveckling, sjukdomar, ekologi och evolution av växter.',
@@ -24,9 +46,14 @@ describe('Search', () => {
         expect(result[0].url).toBe('https://sv.wikipedia.org/wiki/Botanik');
         expect(result[1].url).toBe('https://sv.wikipedia.org/wiki/Växt');
     });
-    // test('Single adjecent words', () => {
-    //     const result = engine.search('botanik de');
-    //     expect(result).toHaveLength(1),
-    //     expect(result[0].url).toBe('https://sv.wikipedia.org/wiki/Växt');
-    // });
+    test('Single adjecent words', () => {
+        const result = engine.search('botanik de');
+        expect(result).toHaveLength(1),
+        expect(result[0].url).toBe('https://sv.wikipedia.org/wiki/Växt');
+    });
+    test('Single words', () => {
+        const result = engine.search('botanik biologiska divisioner');
+        expect(result).toHaveLength(1),
+        expect(result[0].url).toBe('https://sv.wikipedia.org/wiki/Växt');
+    });
 })
--- engine.ts
+++ engine.ts
@@ -3,47 +3,104 @@ export interface SearchResult {
     url: string;
 }
 
-export interface SearchWord {
+export interface Site {
     url: string;
-    index: number;
-}
-
-export interface SearchWordExtended extends SearchWord {
-    word: string;
 }
 
 export class Engine {
-    index: Record<string, SearchWord[]>;
+    index: Record<string, number[]>;
+    site: Record<number, Site>;
+    urlToSite: Record<string, number>;
+    seed: number;
 
     constructor() {
         this.index = {};
+        this.site = {};
+        this.urlToSite= {};
+        this.seed = 0;
     }
 
     add({text, url}: {text: string, url : string}) {
+        const siteKey = `site:${url}`;
+        if (!this.urlToSite[url]) {
+            this.urlToSite[url] = this.seed;
+            this.site[this.seed] = {
+                url
+            };
+            this.index[siteKey] = [];
+        }
+        
         this.toWords(text).forEach((word, index) => {
-            const lowerWord = word.toLowerCase();
-            this.index[lowerWord] = this.index[lowerWord] || [];
-            this.index[lowerWord].push({ url, index });
+            const wordKey = word.toLowerCase();
+            
+            if (!this.index[wordKey])
+                this.index[wordKey] = [];
+            this.index[wordKey].push(this.seed);
+            this.index[siteKey].push(this.seed);
         });
+
+        this.seed += 1;
     }
 
     search(text: string): SearchResult[] {
         const words = this.toWords(text).map(word => word.toLowerCase());
-        const usedUrls = {};
-        const searchWords: SearchWordExtended[][] = words.map((word, index) => {
-            const resultWords = this.index[word];
-            if (!resultWords || resultWords.length === 0) return;
-            const results = resultWords.map(resultWord => {
-                return { word, ...resultWord } as SearchWordExtended;
-            });
-            return results.map(result => {
-                if (usedUrls[result.url]) return;
-                usedUrls[result.url] = true;
-                return result;
-            }).filter(result => Boolean(result));
-        });
+        const arrs = words.map((word) => {
+            return this.uniqueArr(this.index[word] || []);
+        })
+        .filter(arr => arr.length > 0);
 
-        return searchWords[0].map(word => ({ url: word.url, ingress: word.word }));
+        const result = this.intersectMax(arrs, 5);
+        return this.uniqueArr(result).map(site => {
+            return {
+                ingress: '',
+                url: this.site[site].url
+            };
+        });
+    }
+
+    private uniqueArr(arr: number[]): number[] {
+        return [...new Set(arr)];
+    }
+
+    private intersectMax(arrs: number[][], maxCount: number): number[] {
+        if (arrs.length === 0) return [];
+        if (arrs.length === 1) return arrs[0];
+        const result: number[] = [];
+        let indices = new Array(arrs.length).fill(0);
+        while (result.length < maxCount) {
+            let vals = [];
+            for (let i = 0; i < arrs.length; i++) {
+                const ind = indices[i];
+                if (ind >= arrs[i].length) return result;
+                vals.push(arrs[i][ind]);
+            }
+            if (this.isAllEqual(vals))
+                result.push(vals[0]);
+            const minValue = Math.min(...vals);
+            const arrIndexWithSmallestValue = vals.indexOf(minValue);
+            indices[arrIndexWithSmallestValue] += 1;
+        }
+        return result;
+    }
+
+    private sumArray(vals: number[]): number {
+        return vals.reduce((av, cv) => {
+            return av + cv;
+        }, 0);
+    }
+
+    private isAllEqual(vals: number[]) {
+        return this.sumArray(vals) / vals.length == vals[0];
+    }
+
+    private binarySearch(arr: number[], value: number): number {
+        const len = arr.length;
+        if (len === 1) return arr[0];
+        const index = Math.floor(len / 2);
+        if (arr[index] < value)
+            return this.binarySearch(arr.slice(0, index), value);
+        else
+            return this.binarySearch(arr.slice(index), value);
     }
 
     private toWords (text: string): string[] {
